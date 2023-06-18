2cbe47cd51d2deded3674d3942ad533a108e84ae
lochuhsin <lochuhsin@gmail.com>
2023-06-18 14:48:27 +0800

    feat(qles): refactor structure

--- qles/build.go
+++ qles/build.go
@@ -4,30 +4,87 @@ import (
 	"github.com/xwb1989/sqlparser"
 )
 
-func BuildSQL(sqlQuery string) (sqlparser.SQLNode, error) {
+/*Current only support single table query and expect no alias*/
+func BuildES(sqlQuery string, pathMap map[string]string) (Query, error) {
+
+	ast, err := preprocess(sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+	selectAST := ast.(*sqlparser.Select)
+	// search
+	searchQ := ConvertWhereToES(selectAST.Where, pathMap)
+	// order by
+	sortQ := ConvertOrderByToES(selectAST.Where, selectAST.OrderBy, pathMap)
+	return GetESQuery(searchQ, sortQ), nil
+}
+
+func preprocess(sqlQuery string) (sqlparser.SQLNode, error) {
 	parsed, err := sqlparser.Parse(sqlQuery)
 	if err != nil {
 		return nil, err
 	}
-	return parsed, nil
-}
-
-func ReverseNot(node sqlparser.SQLNode) sqlparser.SQLNode {
-	selectStatement := node.(*sqlparser.Select)
+	selectStatement := parsed.(*sqlparser.Select)
 	whereNode := selectStatement.Where
 	if whereNode != nil {
 
 		whereNode.Expr = reverseTree(whereNode.Expr, false)
 	}
 	selectStatement.Where = whereNode
-	return selectStatement
+	return selectStatement, nil
 }
 
-/*Current only support single table query and expect no alias*/
-func BuildESQuery(ast sqlparser.SQLNode, pathMap map[string]string) Query {
-	selectAST := ast.(*sqlparser.Select)
-	// order by
-	searchQ := ConvertWhereToES(selectAST.Where, pathMap)
-	sortQ := ConvertOrderByToES(selectAST.Where, selectAST.OrderBy, pathMap)
-	return GetESQuery(searchQ, sortQ)
+func ConvertOrderByToES(whereAST *sqlparser.Where, orderbyAST sqlparser.OrderBy, pathMap map[string]string) []map[string]SortObject {
+	orderFields := map[string]string{}
+	for _, fieldObj := range orderbyAST {
+		field := fieldObj.Expr.(*sqlparser.ColName).Name.String()
+		orderFields[field] = fieldObj.Direction
+	}
+
+	components := []map[string]SortObject{}
+	for field, order := range orderFields {
+		// normal field
+		if path, ok := pathMap[field]; ok && path == "" {
+			sortObj := GetSortObj(field, "avg", order, nil)
+			components = append(components, sortObj)
+		} else if ok && path != "" {
+			newPathMap := map[string]string{}
+			for k, v := range pathMap {
+				if v == path {
+					newPathMap[k] = ""
+				} else if v != "" {
+					newPathMap[k] = v
+				}
+			}
+			query := ConvertWhereToES(whereAST, newPathMap)
+			var sortObj map[string]SortObject
+			if query == nil {
+				sortObj = GetSortObj(field, "avg", order, nil)
+			} else {
+				nestedSortQ := GetNestedSortQuery(path, query)
+				sortObj = GetSortObj(field, "avg", order, &nestedSortQ)
+			}
+			components = append(components, sortObj)
+		}
+	}
+	return components
+}
+
+func ConvertWhereToES(whereAST *sqlparser.Where, pathMap map[string]string) Query {
+	if whereAST == nil {
+		return nil
+	}
+	esQueryObj, err := buildESRelation(whereAST.Expr, pathMap)
+	if err != nil {
+		panic(err)
+	}
+	if len(esQueryObj) == 1 {
+		return esQueryObj[0].ToQuery()
+	}
+
+	shouldCond := []Query{}
+	for _, esObj := range esQueryObj {
+		shouldCond = append(shouldCond, esObj.ToQuery())
+	}
+	return GetBoolQuery(shouldCond, Should)
 }
--- qles/convert.go
+++ qles/convert.go
@@ -6,61 +6,6 @@ import (
 	"github.com/xwb1989/sqlparser"
 )
 
-func ConvertOrderByToES(whereAST *sqlparser.Where, orderbyAST sqlparser.OrderBy, pathMap map[string]string) []map[string]SortObject {
-	orderFields := map[string]string{}
-	for _, fieldObj := range orderbyAST {
-		field := fieldObj.Expr.(*sqlparser.ColName).Name.String()
-		orderFields[field] = fieldObj.Direction
-	}
-
-	components := []map[string]SortObject{}
-	for field, order := range orderFields {
-		// normal field
-		if path, ok := pathMap[field]; ok && path == "" {
-			sortObj := GetSortObj(field, "avg", order, nil)
-			components = append(components, sortObj)
-		} else if ok && path != "" {
-			newPathMap := map[string]string{}
-			for k, v := range pathMap {
-				if v == path {
-					newPathMap[k] = ""
-				} else if v != "" {
-					newPathMap[k] = v
-				}
-			}
-			query := ConvertWhereToES(whereAST, newPathMap)
-			var sortObj map[string]SortObject
-			if query == nil {
-				sortObj = GetSortObj(field, "avg", order, nil)
-			} else {
-				nestedSortQ := GetNestedSortQuery(path, query)
-				sortObj = GetSortObj(field, "avg", order, &nestedSortQ)
-			}
-			components = append(components, sortObj)
-		}
-	}
-	return components
-}
-
-func ConvertWhereToES(whereAST *sqlparser.Where, pathMap map[string]string) Query {
-	if whereAST == nil {
-		return nil
-	}
-	esQueryObj, err := buildESRelation(whereAST.Expr, pathMap)
-	if err != nil {
-		panic(err)
-	}
-	if len(esQueryObj) == 1 {
-		return esQueryObj[0].ToQuery()
-	}
-
-	shouldCond := []Query{}
-	for _, esObj := range esQueryObj {
-		shouldCond = append(shouldCond, esObj.ToQuery())
-	}
-	return GetBoolQuery(shouldCond, Should)
-}
-
 func mergeAnd(left, right []Object) []Object {
 	if left != nil && right != nil {
 		newArr := []Object{}
